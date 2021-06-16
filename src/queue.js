9b00f112ff5588dc2ca94f173209c51e893a38b5
Caio <caio.dantas@elo7.com>
2021-06-16 15:25:33 -0300

    Implementa métodos de fila ordenada por prioridade

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,33 @@
-let queue = [];
-let size = 0;
 
+class Queue {
+    constructor(){
+        this.queue = [];
+        this.size = 0;
+    }
+
+    isEmpty(){ return this.size == 0 }
+
+    consume(){
+        if(this.isEmpty) return;
+        return this.queue[0];
+    }
+    
+    remove(){
+        if(this.isEmpty) return;
+        this.size--;
+        this.queue.shift();
+    }
+
+    publish(item){
+        let indexOrdenado = 0;
+        for(let i = this.size-1; i >= 0; i--){
+            if(this.queue[i].priority > item.priority){
+                indexOrdenado = i+1;
+            }
+        }
+        this.queue.splice(indexOrdenado, 0, item);
+        this.size++;
+    }
+}
 
 
